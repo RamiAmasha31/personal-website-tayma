01b4a676ddcf3ef5836513c515c8fe802b5d01aa
RamiAmasha31 <ramiamasha84@gmailcom>
2024-10-04 16:05:26 +0300

    styles updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col md:flex-row items-center justify-center h-screen">
+      {/* Right side for the text */}
+      <div className="flex-none w-full md:w-1/3 max-w-md bg-white bg-opacity-75 p-8 rounded  mx-4 mb-4 md:mb-0">
+        <h1 className="text-3xl font-bold">Welcome!</h1>
+        <p className="mt-4 text-lg">
+          I'm Tayma Ibrahim, an Information Systems Expert with a Focus on AI{" "}
+        </p>
+      </div>
+
       {/* Left side for the image */}
       <div
-        className="flex-none w-2/5 h-full bg-cover bg-center " // Image width remains unchanged
+        className="flex-none w-full md:w-2/5 h-64 md:h-full bg-cover bg-center"
         style={{
           backgroundImage: `url('/tayma.png')`, // Adjust this path as necessary
           backgroundSize: "cover", // Scale image to cover the left side
@@ -13,16 +21,6 @@ const Home = () => {
           backgroundPosition: "center",
         }}
       ></div>
-
-      {/* Right side for the text */}
-      <div className="flex-none w-1/3 max-w-md bg-white bg-opacity-75 p-8 rounded shadow-lg mx-4">
-        {" "}
-        {/* Changed width here */}
-        <h1 className="text-3xl font-bold">Welcome!</h1>
-        <p className="mt-4 text-lg">
-          I'm Tayma Ibrahim, an Information Systems Expert with a Focus on AI{" "}
-        </p>
-      </div>
     </div>
   );
 };
